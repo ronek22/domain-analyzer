d93850c7357c61d439f539423be58c6de5b05aa3
Jakub Ronkiewicz <jronek3010@gmail.com>
2019-07-07 21:35:56 +0200

    Set-up Vue frontend

---
+++ client/src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Domain from './components/Domain';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/domains',
+      name: 'Domain',
+      component: Domain,
+    },
+  ],
+});
--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+DEBUG = True
+
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app, resources={r'/*': {'origins': '*'}})
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
