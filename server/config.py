0162e372f5bc86fd0368ae02fa572712a3ffe9d4
Jakub Ronkiewicz <jronek3010@gmail.com>
2019-07-08 23:48:05 +0200

    Working hard on deploying

--- server/config.py
+++ server/config.py
@@ -5,5 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDISTOGO_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost"6379/0'
